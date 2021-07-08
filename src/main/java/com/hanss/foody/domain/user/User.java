0c939011e897b8df1ede75477120e877b729f7ea
hanss2006 <hanss@live.ru>
2021-07-08 23:10:03 +0300

    Добавлена аутентификация

--- src/main/java/com/hanss/foody/domain/user/User.java
+++ src/main/java/com/hanss/foody/domain/user/User.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
--- src/main/java/com/hanss/foody/domain/user/UserRepository.java
+++ src/main/java/com/hanss/foody/domain/user/UserRepository.java
@@ -8,5 +8,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
     public User getUserByName(@Param("name") String name);
+    public User getUserByEmail(@Param("email") String name);
 
 }
