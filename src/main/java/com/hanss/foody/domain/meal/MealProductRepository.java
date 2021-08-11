9322b492e47908fc980db3fb5bb1dd1fdb0cee13
hanss2006 <hanss@live.ru>
2021-08-11 08:30:42 +0300

    Добавлен поиск блюд по типу продукта http://localhost:8080/foody/api/meals/search/findByProductTypeId?id=1

--- src/main/java/com/hanss/foody/domain/meal/MealProductRepository.java
+++ src/main/java/com/hanss/foody/domain/meal/MealProductRepository.java
@@ -1,11 +1,8 @@
 package com.hanss.foody.domain.meal;
 
-import com.hanss.foody.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
-import java.util.List;
 
 @CrossOrigin
 @Repository
--- src/main/java/com/hanss/foody/domain/meal/MealRepository.java
+++ src/main/java/com/hanss/foody/domain/meal/MealRepository.java
@@ -1,6 +1,7 @@
 package com.hanss.foody.domain.meal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @CrossOrigin
 public interface MealRepository extends JpaRepository<Meal, Long> {
     List<Meal> findByName(@Param("name") String name);
+
+    @Query("select m from Meal m left join m.products products where products.id = ?1")
+    List<Meal> findByProductTypeId(Long id);
 }
